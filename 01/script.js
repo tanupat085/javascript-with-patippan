71e33dfab7c24b8c9bed026b3d76a901044a5656
tanupat085 <54698281+tanupat085@users.noreply.github.com>
2020-12-10 22:35:08 +0700

    timer
    start / stop / reset time

---
+++ 01/script.js
@@ -0,0 +1,46 @@
+window.onload = function (){
+    var seconds = 00;
+    var tens = 00 ;
+    var appendtens = document.getElementById("tens");
+    var appendseconds = document.getElementById("seconds");
+
+    var buttonstart = document.getElementById("button-start");
+    var buttonstop = document.getElementById("button-stop");
+    var buttonreset = document.getElementById("button-reset");
+
+    var Interval ;
+
+    buttonstart.onclick = function (){
+        clearInterval(Interval);
+        Interval = setInterval(startTimer,10);
+    }
+    buttonstop.onclick = function (){
+        clearInterval(Interval);
+    }
+    buttonreset.onclick = function(){
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
+        appendtens.innerHTML = tens ;
+        appendseconds.innerHTML = seconds ;
+    }
+    function startTimer(){
+        tens ++;
+        if (tens < 9 ){
+            appendtens.innerHTML = "0" + tens;
+        }
+        if (tens > 9) {
+            appendtens.innerHTML = tens ;
+        }
+        if (tens > 99){
+            seconds ++ ;
+            appendseconds.innerHTML = "0" + seconds;
+            tens = 0 ;
+            appendtens.innerHTML = "0" + 0;
+        }
+        if (seconds > 9 ){
+            appendseconds.innerHTML = seconds;
+        }
+    }
+
+}
